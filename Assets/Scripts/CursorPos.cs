6330741c4c4d5a6279df55bfc8f82f7b7f7bc109
SteamOS User <1027859@student.sae.edu.au>
2026-06-04 20:22:24 +1000

    Implementation 100%?

--- Assets/Scripts/CursorPos.cs
+++ Assets/Scripts/CursorPos.cs
@@ -12,6 +12,7 @@ public class CursorPos : MonoBehaviour
     void Start()
     {
         Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Confined;
     }
 
     // Update is called once per frame
