ed45dae6746e24b0037a9e1bdd36ffaa7cb5a8d5
SteamOS User <1027859@student.sae.edu.au>
2026-06-26 20:47:03 +1000

    added AMM3K + GlorpNodes

---
+++ Assets/AudiophileMegaMixer3000.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+
+public class AudiophileMegaMixer3000 : MonoBehaviour
+{
+    public AudioClip[] ambientSounds;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        foreach (AudioClip clip in ambientSounds)
+        {
+            GameObject glorpNode = new GameObject
+            {
+                transform =
+                {
+                    parent = transform
+                },
+                name = "Glorp Node"
+            };
+            glorpNode.AddComponent<AudioSource>().clip = clip;
+            glorpNode.GetComponent<AudioSource>().loop = true;
+            glorpNode.GetComponent<AudioSource>().Play();
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
