1420a94faa8c5f38d2c3dcfcf0ae0cf44535b005
SteamOS User <1027859@student.sae.edu.au>
2026-06-26 20:55:38 +1000

    clooornk

--- Assets/Scripts/MenuManager.cs
+++ Assets/Scripts/MenuManager.cs
@@ -4,6 +4,8 @@ using UnityEngine.SceneManagement;
 public class MenuManager : MonoBehaviour
 {
     public bool mouseAvailable;
+
+    public AudioClip clooornk;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -28,20 +30,24 @@ public class MenuManager : MonoBehaviour
     public void OPEN_SCENE_INDEX(int sceneIndex)
     {
         SceneManager.LoadScene(sceneIndex);
+        FindFirstObjectByType<SoundBlaster98>().TriggerSound(clooornk);
     }
 
     public void RELOAD_SCENE()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        FindFirstObjectByType<SoundBlaster98>().TriggerSound(clooornk);
     }
 
     public void QUIT_GAME()
     {
         Application.Quit();
+        FindFirstObjectByType<SoundBlaster98>().TriggerSound(clooornk);
     }
 
     public void ENABLE_OBJECT(GameObject obj)
     {
         obj.SetActive(true);
+        FindFirstObjectByType<SoundBlaster98>().TriggerSound(clooornk);
     }
 }
