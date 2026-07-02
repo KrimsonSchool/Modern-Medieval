bf4a41703f2776bdf2ec143e158f2174e604bd8a
SteamOS User <1027859@student.sae.edu.au>
2026-07-02 21:21:16 +1000

    build work

--- Assets/Scripts/EnemiesQuest.cs
+++ Assets/Scripts/EnemiesQuest.cs
@@ -8,29 +8,43 @@ public class EnemiesQuest : MonoBehaviour
     public TextMeshProUGUI enemiesQuestText;
 
     private PDA pda;
+    
+    WorldManager worldManager;
+    
+    [Space] SoundBlaster98 sound;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        sound = FindFirstObjectByType<SoundBlaster98>();
+        worldManager = FindFirstObjectByType<WorldManager>();
         pda = FindFirstObjectByType<PDA>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (pda.menu == PDA.Menus.Goals)
+        if (pda != null)
         {
-            pda.stack.text = "";
-            print("on goals menu");
-            //pda.stackLock = true;
-            print("stack was: "+pda.stack.text);
-            pda.stack.text = "Enemies killed: " + killedEnemies +"/" + noOfEnemies;
-            print("stack is now: "+pda.stack.text);
+            if (pda.menu == PDA.Menus.Goals)
+            {
+                pda.stack.text = "";
+                print("on goals menu");
+                //pda.stackLock = true;
+                print("stack was: "+pda.stack.text);
+                pda.stack.text = "Enemies killed: " + killedEnemies +"/" + noOfEnemies;
+                print("stack is now: "+pda.stack.text);
+            }
         }
-        
+        else
+        {
+            pda = FindFirstObjectByType<PDA>();
+        }
         
         if (killedEnemies >= noOfEnemies)
         {
+            sound.TriggerSound(worldManager.sounds[6]);
             GetComponent<DoorOpener>().OpenDoor();
+            enabled = false;
         }
     }
 }
