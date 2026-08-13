576409c3187574ae6ce124c87ba6ff1651de0512
SteamOS User <1027859@student.sae.edu.au>
2026-08-13 17:36:34 +1000

    level select

--- Assets/Scripts/DEBUGMENUPICKER.cs
+++ Assets/Scripts/DEBUGMENUPICKER.cs
@@ -1,15 +1,20 @@
 using TMPro;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class DEBUGMENUPICKER : MonoBehaviour
 {
      TMP_InputField inputField;
      MenuManager menuManager;
+     
+     TMP_Dropdown dropdown;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         inputField = FindFirstObjectByType<TMP_InputField>();
         menuManager = FindFirstObjectByType<MenuManager>();
+
+        dropdown = GetComponent<TMP_Dropdown>();
     }
 
     // Update is called once per frame
@@ -22,4 +27,10 @@ public class DEBUGMENUPICKER : MonoBehaviour
     {
         menuManager.OPEN_SCENE(inputField.text);
     }
+
+    public void SetScene()
+    {
+        SceneManager.LoadScene(dropdown.value);
+    }
+    
 }
